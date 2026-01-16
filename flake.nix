e163d075d4912e75396ef5d1e6e67bc4dbee3214
Gregory Foster <brona90@gmail.com>
2026-01-16 16:40:01 -0500

    fix: starship vi-mode symbols and update inputs

--- flake.nix
+++ flake.nix
@@ -132,8 +132,12 @@
                 settings = {
                   add_newline = true;
                   character = {
-                    success_symbol = "[➜](bold green)";
-                    error_symbol = "[➜](bold red)";
+                    success_symbol = "[>](bold green)";
+                    error_symbol = "[>](bold red)";
+                    vimcmd_symbol = "[<](bold green)";
+                    vimcmd_replace_one_symbol = "[<](bold purple)";
+                    vimcmd_replace_symbol = "[<](bold purple)";
+                    vimcmd_visual_symbol = "[<](bold yellow)";
                   };
                   directory = {
                     truncation_length = 3;
