23b786bd697de8fb71c1a517485b07ff19643f95
Gregory Foster <brona90@gmail.com>
2026-01-20 09:40:14 -0500

    feat(tmux): add tmux module wrapper

---
+++ modules/tmux/default.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.my.tmux;
+in
+{
+  options.my.tmux = {
+    enable = mkEnableOption "Gregory's tmux configuration (gpakosz/.tmux)";
+
+    configDir = mkOption {
+      type = types.path;
+      description = "Path to the tmux config directory containing .tmux.conf and .tmux.conf.local";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [
+      pkgs.perl  # Required for tmux plugins
+    ];
+
+    programs.tmux = {
+      enable = true;
+      extraConfig = ''
+        # Source the gpakosz config
+        source-file ${cfg.configDir}/.tmux.conf
+      '';
+    };
+
+    # Set TMUX_CONF environment variable for the config to find itself
+    home.sessionVariables = {
+      TMUX_CONF = "${cfg.configDir}/.tmux.conf";
+    };
+  };
+}
