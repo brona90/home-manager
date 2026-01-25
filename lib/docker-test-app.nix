401c1d17710946be35c5f03ec4a84848b8698fc1
Gregory Foster <brona90@gmail.com>
2026-01-24 20:11:47 -0500

    refactor(lib): extract docker-test app to lib/docker-test-app.nix

---
+++ lib/docker-test-app.nix
@@ -0,0 +1,28 @@
+# Docker test app - runs the built image locally
+#
+# Usage: nix run .#docker-test
+
+{ pkgs, homeDirectory, imageName ? "brona90/terminal", imageTag ? "latest" }:
+
+{
+  type = "app";
+  program = toString (pkgs.writeShellScript "docker-test" ''
+    set -e
+    echo "Building Docker image..."
+    rm -f result
+    nix build ~/.config/home-manager#dockerImage
+
+    echo "Loading image into Docker..."
+    docker load < result
+
+    DOCKER_ARGS="-it --rm --network host"
+    DOCKER_ARGS="$DOCKER_ARGS --tmpfs ${homeDirectory}:exec,uid=1000,gid=1000,mode=0755"
+    DOCKER_ARGS="$DOCKER_ARGS --tmpfs /tmp:exec,mode=1777"
+
+    [ -d "$HOME/.ssh" ] && DOCKER_ARGS="$DOCKER_ARGS -v $HOME/.ssh:${homeDirectory}/.ssh:ro"
+    [ -n "$SSH_AUTH_SOCK" ] && DOCKER_ARGS="$DOCKER_ARGS -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
+
+    echo "Starting container..."
+    docker run $DOCKER_ARGS ${imageName}:${imageTag}
+  '');
+}
